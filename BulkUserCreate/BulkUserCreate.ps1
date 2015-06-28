a8721ac9955d64d1da2f2868c5343dff15cde501
Mike Garuccio <mgaruccio@gmail.com>
2015-06-27 21:54:11 -0400

    Fixed Typo

--- BulkUserCreate/BulkUserCreate.ps1
+++ BulkUserCreate/BulkUserCreate.ps1
@@ -7,4 +7,6 @@ foreach($user in $UserFile){
     $FullName = $User.First_Name+" "+$User.Last_Name
 
     New-ADUser -Name $FullName -DisplayName $FullName -GivenName $User.First_Name -Surname $User.Last_Name -UserPrincipalName $User.User_Name -SamAccountName $User.User_name -Path "OU=BasicUsers,DC=LAB,DC=Poshscript,DC=Com" -AccountPassword $Password -Enabled $True
+    $Password = $Null
+    write-host "$FullName has been added to active directory"
 }
