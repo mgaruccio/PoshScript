97fc3df79d48d7e9ad77d35f96c920fb58141f0a
mgaruccio <mgaruccio@gmail.com>
2015-07-09 00:02:25 -0400

    adding CPUUsage code

---
+++ cpuUsage/CpuUsage.ps1
@@ -0,0 +1,48 @@
+﻿
+Function get-cpuUsage{
+
+    Param(
+        [Parameter(Position=1)]
+        $computerName = $env:COMPUTERNAME
+      )
+
+    $Processes = [diagnostics.process]::GetProcesses()
+
+
+    $ProcNum = (Get-WmiObject win32_processor -Property numberoflogicalprocessors).numberoflogicalprocessors
+
+    $arr1 = @()
+
+    foreach ($process in $Processes){
+        $arr1 += [PsCustomObject]@{
+            name = $process.ProcessName
+            cpu = $process.cpu
+        }
+    }
+
+    Start-Sleep -Seconds 1
+
+    $arr2 = @()
+
+    foreach ($process in $Processes){
+        $arr2 += [PsCustomObject]@{
+            name = $process.ProcessName
+            cpu = $process.cpu
+        }
+    }
+
+    $return = @()
+
+
+    for($i=0;$i -le $arr1.count;$i++){
+        $return += [PSCustomObject]@{
+            name = $arr1[$i].name
+            time = ($arr2[$i].cpu - $arr1[$i].cpu) / $ProcNum * 100
+        }
+    }
+
+    $return
+
+}
+
+get-cpuUsage
