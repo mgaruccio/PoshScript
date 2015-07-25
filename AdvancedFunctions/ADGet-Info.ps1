c9158bde088bc615586f56a2f333d9f7fabeb40b
mgaruccio <mgaruccio@gmail.com>
2015-07-25 12:37:11 -0400

    adding Message Box function

---
+++ AdvancedFunctions/ADGet-Info.ps1
@@ -0,0 +1,21 @@
+﻿Function Get-Info{
+    param(
+        [Parameter(Mandatory=$True,Position=1)]
+          [string]$FilePath
+    )
+    BEGIN{
+        if(test-path $FilePath){
+            $ServerList = Get-Content $FilePath
+            $i = 0
+        }
+        else{
+            return "Could not find file "+$FilePath
+        }
+    }
+    PROCESS{
+        write-host $ServerList[$i]
+    }
+    END{
+
+    }
+}
---
+++ messagebox/messageboxFunction.ps1
@@ -0,0 +1,47 @@
+﻿Function Generate-messagebox(){
+    
+    [cmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,Position=1)]
+          [string]$message,
+        [Parameter(Mandatory=$False,Position=2)]
+          [string]$header = "Information",
+        [Parameter(Mandatory=$False,Position=3)]
+          [string]$buttonType = "OK",
+        [Parameter(Mandatory=$False,Position=4)]
+          [string]$icon = "None",
+        [Parameter(Mandatory=$False,Position=5)]
+          [int32]$timeout = 0
+    )
+
+    [int32]$type = 0
+    [int32]$iconInt = 0
+
+    if($buttonType){
+        Switch($buttonType){
+            OK {$type = 0}
+            OKCancel {$type = 1}
+            AbortRetryIgnore {$type = 2}
+            YesNoCancel {$type = 3}
+            YesNo {$type = 4}
+            RetryCancel {$type = 5}
+            Default {Throw "ButtonType is not set to an acceptable value"}
+        }
+    }
+    if($icon){
+        Switch($icon){
+            None {$iconInt = 0}
+            Stop {$iconInt = 16}
+            Question {$iconInt = 32}
+            Exclamation {$iconInt = 48}
+            Information {$iconInt = 64}
+            Default {Throw "IconType is not set to an acceptable value"}
+        }
+    }
+
+    $INT = $Type+$iconInt
+
+
+    $wshell = New-Object -ComObject Wscript.Shell
+    return $wshell.Popup($message, $timeout, $header, $INT)
+}
