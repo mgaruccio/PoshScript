ed1f7f19a85f0e76a9fc35012022c2b09c694da2
mgaruccio <mgaruccio@gmail.com>
2015-07-26 16:05:00 -0400

    adding WPF input boxes

---
+++ inputbox/inputboxexample.ps1
@@ -0,0 +1,10 @@
+﻿function get-info($computerName){
+    $return = "" | Select-Object OS,Driver,Disk,Share
+        
+    $return.OS = Get-WmiObject Win32_OperatingSystem -ComputerName $Computer.name
+    $return.Driver = Get-WmiObject Win32_SystemDriver -ComputerName $Computer.name
+    $return.Disk = Get-WmiObject Win32_logicalDisk -ComputerName $Computer.name
+    $return.share = Get-WmiObject win32_share -ComputerName $Computer.name
+
+    return $Return
+}
