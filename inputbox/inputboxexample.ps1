e71e5c9b933ea210c94c7ac1809626bd7d22c871
mgaruccio <mgaruccio@gmail.com>
2015-08-01 11:38:35 -0400

    adding inputbox function for testing

--- inputbox/inputboxexample.ps1
+++ inputbox/inputboxexample.ps1
@@ -1,4 +1,4 @@
-﻿function get-info($computerName){
+﻿Function get-info($computerName){
     $return = "" | Select-Object OS,Driver,Disk,Share
         
     $return.OS = Get-WmiObject Win32_OperatingSystem -ComputerName $Computer.name
@@ -8,3 +8,56 @@
 
     return $Return
 }
+
+Function Load-Dialog($Window){
+
+    try{
+        Add-Type -AssemblyName PresentationCore,PresentationFramework,WindowsBase,system.windows.forms
+    }
+    catch{
+        Throw "Failed to load the Presentation Assembly"
+    }
+
+    $Global:xamGUI = [Windows.Markup.XamlReader]::Load((new-object System.Xml.XmlNodeReader $window))
+
+    $window.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name ($_.Name) -Value $xamGUI.FindName($_.Name) -Scope Global}
+}
+
+$xml = [xml] @"
+<Window 
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"        
+        Title="MainWindow" Height="120" Width="525">
+    <Grid>
+        <TextBox Name="txtInputFile" HorizontalAlignment="Left" Height="41" Margin="10,10,0,0" TextWrapping="Wrap" Text="Input File" VerticalAlignment="Top" Width="328" FontSize="16"/>
+        <Button Name="btnSelectFile" Content="Select File" HorizontalAlignment="Left" Margin="354,10,0,0" VerticalAlignment="Top" Width="112" Height="41"/>
+        <Button Name="btnGenerate" Content="Generate Report" HorizontalAlignment="Left" Margin="10,56,0,0" VerticalAlignment="Top" Width="500"/>
+
+    </Grid>
+</Window>
+
+
+"@
+
+
+
+Load-Dialog $xml
+
+$btnSelectFile.add_click({
+    $dialog = New-Object System.Windows.Forms.OpenFileDialog
+    $dialogResult = $dialog.ShowDialog()
+    if($dialogResult -eq "OK"){
+        $txtInputFile.Text = $dialog.filename
+    }
+})
+
+$btnGenerate.add_click({
+    $Computers = get-content $txtInputFile.Text
+    $results = @()
+    Foreach($computer in $computers){
+        $results += get-info $computer
+    }
+    $results | export-csv "results_$(get-date -f MM-dd-yyyy).csv"
+})
+
+$xamGui.ShowDialog() | Out-Null
