f8ce7cd945e67fd18ba213b072eec22138e29e31
mgaruccio <mgaruccio@gmail.com>
2015-08-02 19:33:26 -0400

    inputbox function

--- inputbox/inputboxexample.ps1
+++ inputbox/inputboxexample.ps1
@@ -24,6 +24,54 @@ Function Load-Dialog($Window){
     $window.SelectNodes("//*[@Name]") | ForEach-Object {Set-Variable -Name ($_.Name) -Value $xamGUI.FindName($_.Name) -Scope Global}
 }
 
+Function Generate-messagebox(){
+    
+    [cmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,Position=1)]
+          [string]$message,
+        [Parameter(Mandatory=$False,Position=2)]
+          [string]$header = "Information",
+        [Parameter(Mandatory=$False,Position=3)]
+          [string]$buttonType = "OK",
+        [Parameter(Mandatory=$False,Position=4)]
+          [string]$icon = "None",
+        [Parameter(Mandatory=$False,Position=5)]
+          [int32]$timeout = 0
+    )
+
+    [int32]$type = 0
+    [int32]$iconInt = 0
+
+    if($buttonType){
+        Switch($buttonType){
+            OK {$type = 0}
+            OKCancel {$type = 1}
+            AbortRetryIgnore {$type = 2}
+            YesNoCancel {$type = 3}
+            YesNo {$type = 4}
+            RetryCancel {$type = 5}
+            Default {Throw "ButtonType is not set to an acceptable value"}
+        }
+    }
+    if($icon){
+        Switch($icon){
+            None {$iconInt = 0}
+            Stop {$iconInt = 16}
+            Question {$iconInt = 32}
+            Exclamation {$iconInt = 48}
+            Information {$iconInt = 64}
+            Default {Throw "IconType is not set to an acceptable value"}
+        }
+    }
+
+    $INT = $Type+$iconInt
+
+
+    $wshell = New-Object -ComObject Wscript.Shell
+    return $wshell.Popup($message, $timeout, $header, $INT)
+}
+
 $xml = [xml] @"
 <Window 
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
@@ -59,6 +107,7 @@ $btnGenerate.add_click({
         $results += get-info $Computer
     }
     $results | export-csv "results_$(get-date -f MM-dd-yyyy).csv"
+    Generate-messagebox "report generated"
 })
 
 $xamGui.ShowDialog() | Out-Null
